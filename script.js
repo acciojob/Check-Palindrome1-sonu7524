e6195cb585632d02709572502fe0d6b2c2f8f0ed
Sonu Daryani <100096513+sonu7524@users.noreply.github.com>
2023-05-14 19:41:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-// complete the given function
-
 function palindrome(str){
-
+	const s = str.replaceAll(" ","");
+	const ch = s.split('');
+	let l=0;
+	let r=ch.length-1;
+	while(l<r){
+		if(ch[l]!== ch[r]) return false;
+		l++;
+		r--;
+	}
+	return true;
 }
 module.exports = palindrome
